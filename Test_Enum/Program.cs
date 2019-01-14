5d59c2394ac6b102d74c6ca27dedfb8e94f7d521
PierrotAix <pdesmazes@gmail.com>
2019-01-14 11:06:48 +0100

    Added Test_LINQ_to_XML

---
+++ Test_Enum/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Enum
+{
+    /// <summary>
+    /// Question 145 Objective: Implement Data Access Subobjective: Store data and retrieve data from collections.
+    /// </summary>
+    class Program
+    {
+
+        public enum Planet
+        {
+            Mercury,
+            Venus,
+            Earth,
+            Mars,
+            Jupiter,
+            Saturn,
+            Uranus,
+            Neptune,
+            PlutoDwarf
+        }
+        public class SolarSystem
+        {
+            public static IEnumerable<Planet> PlanetByDistanceFromSun
+            {
+                get
+                {
+                    yield return Planet.Mercury;
+                    yield return Planet.Venus;
+                    yield return Planet.Earth;
+                    yield return Planet.Mars;
+                    yield return Planet.Jupiter;
+                    yield return Planet.Saturn;
+                    yield return Planet.Uranus;
+                    yield return Planet.Neptune;
+                    yield return Planet.PlutoDwarf;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            foreach (Planet p in SolarSystem.PlanetByDistanceFromSun)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Test_LINQ_to_XML/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Test_LINQ_to_XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xDoc = XDocument.Load("monFichier.xml");
+
+            var result = xDoc.Descendants("Movie");
+
+            var movies = from m in xDoc.Descendants("Movie")
+                         where m.Attribute("ReleaseDate").Value == ""
+                         select m;
+
+            movies.ToList().ForEach(m => m.Attribute("ReleaseDate").Value = "2013"); //ne marche pas
+
+            foreach (var movie in movies)
+            {
+                Console.WriteLine(movie.Descendants("Title"));
+            }
+
+            Console.WriteLine("Tapez unr touche pour finir");
+            Console.ReadLine();
+        }
+    }
+}
